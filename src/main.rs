33b97d836f3396bc3487d5e7e39f60fbe05e9013
imkgerC <imrund08@gmail.com>
2020-05-29 00:24:37 +0200

    add report

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,42 @@ use std::time::Instant;
 fn main() {
     // list lengths
     let sizes: [usize; 3] = [1000, 10_000, 100_000];
-    let random_samples = 1;
-    let mut sorts: Vec<(Box<dyn Fn(&mut [usize])>, String)> = vec![(Box::new(quick_sort), "bubble".to_owned())];
+    let random_samples = 10;
+    let mut sorts: Vec<(Box<dyn Fn(&mut Vec<usize>)>, String)> = vec![
+        (Box::new(bubble_sort), "bubble".to_owned()),
+        (Box::new(merge_sort), "merge".to_owned()),
+        (Box::new(heap_sort), "heap".to_owned()),
+        (Box::new(shaker_sort), "shaker".to_owned()),
+        (Box::new(radix_sort), "radix".to_owned()),
+        (Box::new(selection_sort), "selection".to_owned()),
+        (Box::new(insertion_sort), "insertion".to_owned()),
+        (Box::new(shell_sort), "shell".to_owned()),
+        (Box::new(quick_sort), "quick".to_owned()),
+    ];
     for size in &sizes {
-        let vals = (0..(*size)).map(|_| thread_rng().gen()).collect::<Vec<_>>();
+        // sorted
+        let mut vals = (0..(*size)).collect();
         for (sort, name) in &mut sorts {
-            println!("{} {} {}", name, size, measure(&vals, sort));
+            println!("sorted {} {} {}", name, size, measure(&vals, sort));
+        }
+        // inversely sorted
+        vals.reverse();
+        for (sort, name) in &mut sorts {
+            println!("inverse {} {} {}", name, size, measure(&vals, sort));
+        }
+        // random
+        for (sort, name) in &mut sorts {
+            let mut sum = 0;
+            for _ in 0..random_samples {
+                let vals = (0..(*size)).map(|_| thread_rng().gen()).collect::<Vec<_>>();
+                sum += measure(&vals, sort)
+            }
+            println!("random {} {} {}", name, size, sum / random_samples);
         }
     }
 }
 
-fn bubble_sort(arr: &mut [usize]) {
+fn bubble_sort(arr: &mut Vec<usize>) {
     for j in 0..(arr.len() - 1) {
         for i in 1..(arr.len() - j) {
             if arr[i - 1] > arr[i] {
@@ -27,20 +52,20 @@ fn bubble_sort(arr: &mut [usize]) {
 }
 
 fn merge_sort(arr: &mut Vec<usize>) {
-    mergesort(arr, 0, arr.len()-1)
+    mergesort(arr, 0, arr.len() - 1)
 }
 
 pub fn mergesort(arr: &mut Vec<usize>, b: usize, e: usize) {
     if b < e {
-        let m = (b+e)/2;
+        let m = (b + e) / 2;
         mergesort(arr, b, m);
-        mergesort(arr, m+1, e);
+        mergesort(arr, m + 1, e);
         merge(arr, b, m, e);
     }
 }
-fn merge(arr: &mut Vec<usize>, b: usize, m:usize, e:usize) {
-    let mut left = arr[b..m+1].to_vec();
-    let mut right = arr[m+1..e+1].to_vec();
+fn merge(arr: &mut Vec<usize>, b: usize, m: usize, e: usize) {
+    let mut left = arr[b..m + 1].to_vec();
+    let mut right = arr[m + 1..e + 1].to_vec();
     left.reverse();
     right.reverse();
     for k in b..e + 1 {
@@ -54,14 +79,13 @@ fn merge(arr: &mut Vec<usize>, b: usize, m:usize, e:usize) {
         }
         if right.last() < left.last() {
             arr[k] = right.pop().unwrap();
-        }
-        else {
+        } else {
             arr[k] = left.pop().unwrap();
         }
     }
 }
 
-fn radix_sort(arr: &mut [usize]) {
+fn radix_sort(arr: &mut Vec<usize>) {
     let mut max = 0;
     for i in 0..arr.len() {
         if arr[i] > max {
@@ -72,7 +96,7 @@ fn radix_sort(arr: &mut [usize]) {
     for i in 0..bits {
         let mut ones: Vec<usize> = Vec::with_capacity(arr.len());
         let mut zeros: Vec<usize> = Vec::with_capacity(arr.len());
-        
+
         for j in 0..arr.len() {
             if (arr[j] & (1 << i)) > 0 {
                 ones.push(arr[j]);
@@ -80,7 +104,7 @@ fn radix_sort(arr: &mut [usize]) {
                 zeros.push(arr[j]);
             }
         }
-        
+
         let mut j = 0;
         for x in zeros {
             arr[j] = x;
@@ -94,7 +118,7 @@ fn radix_sort(arr: &mut [usize]) {
     }
 }
 
-fn counting_sort(arr: &mut [usize]) {
+fn counting_sort(arr: &mut Vec<usize>) {
     let mut max = 0;
     for i in 0..arr.len() {
         if arr[i] > max {
@@ -111,15 +135,15 @@ fn counting_sort(arr: &mut [usize]) {
             i += 1;
         }
         buckets[i] -= 1;
-        arr[j] = i+1;
+        arr[j] = i + 1;
     }
 }
 
-fn heap_sort(arr: &mut [usize]) {
+fn heap_sort(arr: &mut Vec<usize>) {
     heapsort(arr, arr.len() as isize)
 }
 
-fn heapsort(arr: &mut [usize], n: isize) {
+fn heapsort(arr: &mut Vec<usize>, n: isize) {
     // Build heap (rearrange array)
     let mut i = n / 2 - 1;
     while i >= 0 {
@@ -139,7 +163,7 @@ fn heapsort(arr: &mut [usize], n: isize) {
     }
 }
 
-fn heapify(arr: &mut [usize], n: usize, i: usize) {
+fn heapify(arr: &mut Vec<usize>, n: usize, i: usize) {
     let mut largest = i; // Initialize largest as root
     let l = 2 * i + 1; // left = 2*i + 1
     let r = 2 * i + 2; // right = 2*i + 2
@@ -163,7 +187,7 @@ fn heapify(arr: &mut [usize], n: usize, i: usize) {
     }
 }
 
-fn selection_sort(arr: &mut [usize]) {
+fn selection_sort(arr: &mut Vec<usize>) {
     for i in 0..(arr.len() - 1) {
         let mut x = arr[i];
         let mut k = i;
@@ -178,8 +202,8 @@ fn selection_sort(arr: &mut [usize]) {
     }
 }
 
-fn shell_sort(arr: &mut [usize]) {
-    let mut column_count = [
+fn shell_sort(arr: &mut Vec<usize>) {
+    let column_count = [
         2147483647, 1131376761, 410151271, 157840433, 58548857, 21521774, 8810089, 3501671,
         1355339, 543749, 213331, 84801, 27901, 11969, 4711, 1968, 815, 271, 111, 41, 13, 4, 1,
     ];
@@ -197,12 +221,12 @@ fn shell_sort(arr: &mut [usize]) {
     }
 }
 
-fn quick_sort(arr: &mut [usize]) {
+fn quick_sort(arr: &mut Vec<usize>) {
     let len = arr.len() as isize;
     quicksort(arr, 0, len - 1)
 }
 
-fn quicksort(arr: &mut [usize], lo: isize, hi: isize) {
+fn quicksort(arr: &mut Vec<usize>, lo: isize, hi: isize) {
     if lo < hi {
         let p = partition(arr, lo, hi);
         quicksort(arr, lo, p - 1);
@@ -210,8 +234,31 @@ fn quicksort(arr: &mut [usize], lo: isize, hi: isize) {
     }
 }
 
-fn partition(arr: &mut [usize], lo: isize, hi: isize) -> isize {
-    let pivot = arr[hi as usize];
+fn partition(arr: &mut Vec<usize>, lo: isize, hi: isize) -> isize {
+    let pivot_place;
+    let mid = (lo + hi)/2;
+    if arr[lo as usize] < arr[hi as usize] {
+        if arr[mid as usize] < arr[lo as usize] {
+            pivot_place = lo as usize;
+        } else {
+            if arr[mid as usize] < arr[hi as usize] {
+                pivot_place = mid as usize;
+            } else {
+                pivot_place = hi as usize;
+            }
+        }
+    } else {
+        if arr[mid as usize] < arr[hi as usize] {
+            pivot_place = hi as usize;
+        } else {
+            if arr[mid as usize] < arr[lo as usize] {
+                pivot_place = mid as usize;
+            } else {
+                pivot_place = lo as usize;
+            }
+        }
+    }
+    let pivot = arr[pivot_place];
     let mut i = lo;
     for j in lo..hi {
         if arr[j as usize] < pivot {
@@ -219,11 +266,11 @@ fn partition(arr: &mut [usize], lo: isize, hi: isize) -> isize {
             i += 1;
         }
     }
-    arr.swap(i as usize, hi as usize);
+    arr.swap(i as usize, pivot_place);
     i
 }
 
-fn shaker_sort(arr: &mut [usize]) {
+fn shaker_sort(arr: &mut Vec<usize>) {
     let mut swapped = true;
     while swapped {
         swapped = false;
@@ -247,7 +294,7 @@ fn shaker_sort(arr: &mut [usize]) {
     }
 }
 
-fn insertion_sort(arr: &mut [usize]) {
+fn insertion_sort(arr: &mut Vec<usize>) {
     for i in 1..arr.len() {
         let mut j = (i - 1) as isize;
         let x = arr[i];
@@ -263,16 +310,9 @@ fn insertion_sort(arr: &mut [usize]) {
     }
 }
 
-fn measure(arr: &Vec<usize>, sort: &mut Box<dyn Fn(&mut [usize])>) -> u128 {
+fn measure(arr: &Vec<usize>, sort: &mut Box<dyn Fn(&mut Vec<usize>)>) -> u128 {
     let mut arr = arr.clone();
     let now = Instant::now();
     sort.as_ref()(&mut arr);
     now.elapsed().as_micros()
-}
-
-fn invert(arr: &mut [usize]) {
-    let half = arr.len() / 2;
-    for i in 0..half {
-        arr.swap(i, arr.len() - i - 1);
-    }
 }
